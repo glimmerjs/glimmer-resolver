51e4c9811713ebe6625cdd84d8a3f6aca773dc0e
Dan Gebhardt <dan@cerebris.com>
2017-03-16 15:58:24 -0400

    Funnel test dependencies instead of rebuilding them.

--- Brocfile.js
+++ Brocfile.js
@@ -1,13 +1,14 @@
 "use strict";
 
 const build = require('@glimmer/build');
-const buildVendorPackage = require('@glimmer/build/lib/build-vendor-package');
+const funnel = require('broccoli-funnel');
+const path = require('path');
 
 let buildOptions = {};
 
 if (process.env.BROCCOLI_ENV === 'tests') {
   buildOptions.vendorTrees = [
-    buildVendorPackage('@glimmer/di', { external: ['babel-helpers'] })
+    funnel(path.dirname(require.resolve('@glimmer/di/package')), { include: ['dist/amd/es5/**/*.js'] })
   ];
 }
 
