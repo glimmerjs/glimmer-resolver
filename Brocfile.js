f146554eb1512eef5ca4bc40a511249bd4ab8559
Dan Gebhardt <dan@cerebris.com>
2017-03-19 08:14:43 -0400

    Bump @glimmer/di and @glimmer/build

--- Brocfile.js
+++ Brocfile.js
@@ -1,15 +1,5 @@
 "use strict";
 
 const build = require('@glimmer/build');
-const funnel = require('broccoli-funnel');
-const path = require('path');
 
-let buildOptions = {};
-
-if (process.env.BROCCOLI_ENV === 'tests') {
-  buildOptions.vendorTrees = [
-    funnel(path.dirname(require.resolve('@glimmer/di/package')), { include: ['dist/amd/es5/**/*.js'] })
-  ];
-}
-
-module.exports = build(buildOptions);
+module.exports = build({ vendorPackages: ['@glimmer/di'] });
